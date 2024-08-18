23c51d2720752b44629d733ca8cdb434d7050a61
Waproject13 <153232145+Waproject13@users.noreply.github.com>
2024-08-18 21:18:17 +0200

    Add files via upload

---
+++ Checkpoint_3.py
@@ -0,0 +1,29 @@
+
+my_string = "Hola, Joaquin!"
+my_number = "3"
+my_list = ["cuaderno","lapiz","mochila"]
+my_boolean = True
+first_three_letters = my_string[:3]
+print(first_three_letters)
+
+first_list_element = my_list[0]
+print(first_list_element)
+new_number = int(my_number)+10
+print(new_number)
+print(my_list[-1])
+
+names ='harry,alex,susie,jared,gail,conner'
+list_of_names = names.split(',')
+print(list_of_names)
+
+my_first_word = my_string[:4]
+my_first_word_upper =my_first_word.upper()
+new_string=my_first_word_upper + my_string[4:]
+print(new_string)
+
+my_sentence = f"My favorite number is {my_number}."
+print(my_sentence)
+
+print("hello world.")
+
+
